717eccc6f00e45977b55474920a7617bdfe77a1f
Alex <ablanyaalex@yahoo.com>
2021-10-27 22:34:48 +0100

    added authentication and verfication to the user model

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,5 +1,5 @@
-from django.contrib.auth import get_user_model
-from django.forms import fields
+from django.contrib.auth import get_user_model, authenticate
+from django.utils.translation import ugettext_lazy as _
 
 from rest_framework import serializers
 
@@ -8,10 +8,35 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields =('email', 'password', 'name')
+        fields = ('email', 'password', 'name')
         extra_kwargs = {'password':{'write_only':True, 'min_length':5}}
 
     def create(self, validated_data):
         """Create a new user with encryted password and return it"""
-        return get_user_model().objects.crreate_user(**validated_data)
+        return get_user_model().objects.create_user(**validated_data)
     
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user authenication obhect"""
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},    
+        trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        """validate and authenticate the user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password
+        )
+        if not user:
+            msg =_('Unable to authenticate with provided credientials')
+            raise serializers.ValidationErroe(msg, code='authentication')
+        
+        attrs['user'] = user
+        return attrs
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -8,6 +8,7 @@ from rest_framework import status
 
 
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
 
 def craete_user(**params):
     return get_user_model().objects.create_user(**params)
@@ -55,8 +56,36 @@ class PublicUserApiTests(TestCase):
             user_exists = get_user_model().objects.filter(
                 email=payload('email')
             ).exists()
-            self.assertFalsee(user_exists)
+            self.assertFalse(user_exists)
          
+        def test_create_token_for_user(self):
+            """Test that a token is craeted for the user"""
+            payload = {'email': 'alex@yahoo.com', 'password': 'password'}
+            craete_user(**payload)
+            res = self.client.post(TOKEN_URL, payload)
 
+            self.assertIn('token', res.data)
+            self.assertEqual(res.status_code, status.HTTP_200_OK)
 
+        def test_create_token_invaild_credentials(self):
+            """Test token if invalid or not"""
+            payload = {'email': 'alex@yahoo.com', 'password': 'wrong'}
+            craete_user(email='alex@yahoo.com', password="password")
+            res = self.client.post(TOKEN_URL, payload)
 
+            self.assertNotIn('token', res.data)
+            self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+        def test_create_token_no_user(self):
+            """Test that token is not created if user doesnt exist"""
+            payload = {'email': 'alex@yahoo.com', 'password': 'password'}
+            res = self.client.post(TOKEN_URL, payload)
+
+            self.assertNotIn('token', res.data)
+            self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+        def test_create_token_missing_field(self):
+            """Test that email and password are required"""
+            res = self.client.post(TOKEN_URL, {'email': 'one', 'password':''})
+            self.assertNoIn('token', res.data)
+            self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
