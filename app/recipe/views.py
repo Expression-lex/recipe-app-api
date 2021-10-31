fc662a540db22340b6e5abba2880fa80d04bcaaa
Alex <ablanyaalex@yahoo.com>
2021-10-31 16:52:40 +0100

    refactor the for the tag and ingredient viewset

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,39 +6,31 @@ from core.models import Tag, Ingredient
 
 from recipe import serializers
 
-
-class TagViewSet(viewsets.GenericViewSet, 
-                mixins.ListModelMixin, 
-                mixins.CreateModelMixin):
-    """Manage tags in the database"""
+class TagIngredientViewSetAttr(viewsets.GenericViewSet,
+                               mixins.ListModelMixin,
+                               mixins.CreateModelMixin):
+    """This is a class for both Tag and Ingredient viewsets"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
-    queryset = Tag.objects.all()
-    serializer_class = serializers.TagSerializer
 
     def get_queryset(self):
-        """Return objects for the current authenticated user only"""
+        """Retrun object for authenticated user"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
     def perform_create(self, serializer):
         """Create a new tag"""
         serializer.save(user=self.request.user)
+    
 
 
-class IngredientViewSet(viewsets.GenericViewSet,
-                        mixins.ListModelMixin,
-                        mixins.CreateModelMixin):
+class TagViewSet(TagIngredientViewSetAttr):
+    """Manage tags in the database"""
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+
+class IngredientViewSet(TagIngredientViewSetAttr):
     """Manage ingredients in the database"""
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
-
-    def get_queryset(self):
-        """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self, serializer):
-        """Create a new ingredient"""
-        serializer.save(user=self.request.user)
     
